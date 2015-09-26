afb43b502a262bbcc52460cf2fea83684d9b2b22
eksperimental <eksperimental@users.noreply.github.com>
2015-09-26 23:15:54 +0700

    Enum.pad*: final version

--- lib/enum_pad.ex
+++ lib/enum_pad.ex
@@ -1,57 +1,189 @@
 defmodule Experimental.EnumPad do
-  require Logger
-  
   @type t :: Enumerable.t
 
-  @doc """
-  Pads `collection` with `padding` until it reaches `size`.
+  @doc ~S"""
+  Pads `enumerable` with the elements from `padding` until it
+  reaches `size`.
 
-  If `size` is smaller or equal than the length of `collection`, then
-  `collection` is return unchanged.
+  If `size` is smaller or equal than the length of `enumerable`, then
+  `enumerable` is return unchanged.
 
   If `padding` is a function, it will take the last evaluated element
-  in then collection as the only one argument. 
+  in the enumerable as the argument.
+
+  The zipping finishes as soon as there are no more padding elements
+  to be used.
+
+  If no `padding` is provided, it will fill the enumerable with `nil`.
 
   ## Examples
-    iex> Enum.pad([1, 2, 3], 5)
+    iex> pad([1, 2, 3], 5)
     [1, 2, 3, nil, nil]
     
-    iex> Enum.pad([:a, :b, :c], 5, :z)
+    iex> pad([:a, :b, :c], 5, Stream.cycle([:z]))
     [:a, :b, :c, :z, :z]
     
-    iex> Enum.pad([1, 2, 3], 5, &(&1+10))
+    iex> pad([1, 2, 3], 5, &(&1+10))
     [1, 2, 3, 13, 23]
     
-    iex> Enum.pad([1, 2, 3], 2)
+    iex> pad([1, 2, 3], 2)
     [1, 2, 3]
 
+    iex> pad([1, 2, 3], 6, 50..100)
+    [1, 2, 3, 50, 51, 52]
+
+    iex> pad([1, 2, 3], 10, Stream.cycle([0, 1]))
+    [1, 2, 3, 0, 1, 0, 1, 0, 1, 0]
+
+    iex> pad([:a, :aN], 4, &("#{&1}" |> String.reverse
+    ...> |> String.duplicate(2) |> String.to_atom ))
+    [:a, :aN, :NaNa, :aNaNaNaN]
+
   """
-  @spec pad(t, pos_integer, any) :: [any]
-  def pad(collection, size,  padding \\ nil)
+  @spec pad(t, pos_integer, t | (any -> any)) :: [any]
+  def pad(enumerable, size,  padding \\ Stream.cycle([nil])) do
+    count = Enum.count(enumerable)
 
-  def pad([], size, _padding) when is_integer(size),
-    do: []
+    cond do
+      size <= count ->
+        Enum.to_list(enumerable)
 
-  def pad(col, size, _padding) when is_list(col) and is_integer(size) and size <= length(col),
-    do: col
+      # streams are caught as functions by the guard,
+      # and are also enumerables
+      is_function(padding) and not enumerable?(padding) ->
+        do_pad_with_function(enumerable, size, padding)
 
-  def pad(col, size, fun) when is_function(fun) and is_list(col)
-    and is_integer(size) and size > length(col)
-    do
-      start = length(col) + 1
-      [last_elem] = Enum.take(col, -1)
-      Enum.reduce(start..size, {last_elem, Enum.reverse(col)}, fn(_n, {prev, acc}) ->
+      true ->
+        do_pad_with_enumerable(enumerable, size, padding)
+    end
+  end
+
+  @doc """
+  Returns `true` if the given `term` is an enumerable.
+
+  ## Examples
+    iex> enumerable?([1, 2, 3])
+    true
+
+    iex> enumerable?(1..3)
+    true
+
+    iex> enumerable?(Stream.cycle([:elixir]))
+    true
+
+    iex> enumerable?({:elixir})
+    false
+
+    iex> enumerable?(1)
+    false
+
+  """
+  @spec enumerable?(term) :: boolean
+  def enumerable?(term) do
+    try do
+      Enum.take(term, 1)
+    rescue
+      _ -> false
+    else
+      _ -> true
+    end
+  end
+
+  defp do_pad_with_enumerable(enum, size, padding) do
+    padding
+    |> Enum.take(size - Enum.count(enum))
+    |> Enum.into(Enum.to_list(enum))
+  end
+
+  defp do_pad_with_function(enum, size, fun) do
+    count = Enum.count(enum)
+    start = count + 1
+
+    if count == 0 do
+      last_elem = nil
+    else
+      [last_elem] = Enum.take(enum, -1)
+    end
+
+    Enum.reduce(start..size, {last_elem, Enum.reverse(enum)},
+      fn(_n, {prev, acc}) ->
         cur = fun.(prev)
         {cur, [cur | acc]}
       end)
-      |> elem(1)
-      |> Enum.reverse
+    |> elem(1)
+    |> Enum.reverse
   end
 
-  def pad(col, size, padding) when is_list(col) and is_integer(size) and size > length(col) do
-    Stream.cycle([padding])
-    |> Enum.take(size - length(col))
-    |> Enum.into(col)
+  @doc """
+  Shortcut for `pad_zip(enumerable1, enumerable1, padding, padding)`.
+
+  For more information, see `pad_zip/4`.
+
+  ## Examples
+    # Stream.cycle([nil]) is used as the default padding
+    iex(13)> pad_zip(1..5, [:a])
+    [{1, :a}, {2, nil}, {3, nil}, {4, nil}, {5, nil}]
+
+    # Zipping finished earlier because pading is not long enough 
+    iex(12)> pad_zip(1..5, [:a], [nil])
+    [{1, :a}, {2, nil}]
+
+  """
+  @spec pad_zip(t, t, t | (any -> any)) :: [{any, any}]
+  def pad_zip(enumerable1, enumerable2, padding \\ Stream.cycle([nil])) do
+    pad_zip(enumerable1, enumerable2, padding, padding)
   end
 
+  @doc ~S"""
+  Pads enumerables with the elements from `padding` until it
+  reaches `size`.
+
+  The paddings can be either enumerables, or a function with arity 1.
+  If `padding` is a function, it will take the last evaluated element
+  in then enumerable as the only one argument.
+
+  Once enumerables have been padded, they will be zipped into one list
+  of tuples.
+
+  If one enumerable is shorther than the other, it will be filled with
+  padding, provided there are enough padding elements. The zipping
+  will finish as soon as there are no more padding elements to be used.
+
+  For more information about how the padding works, see `pad/3`.
+
+  ## Examples
+    # stream as padding
+    iex> pad_zip(1..5, [:a, :b, :c], Stream.cycle([0]),
+    ...>   Stream.cycle([:z]))
+    [{1, :a}, {2, :b}, {3, :c}, {4, :z}, {5, :z}]
+  
+    # function as padding
+    iex> pad_zip([:a, :aN], 1..4, &("#{&1}" |> String.reverse
+    ...> |> String.duplicate(2) |> String.to_atom))
+    [a: 1, aN: 2, NaNa: 3, aNaNaNaN: 4]
+
+    # Zipping finishes earlier because pading is not long enough 
+    iex(12)> pad_zip(1..5, [:a], [nil], [nil])
+    [{1, :a}, {2, nil}]
+
+  """
+  @spec pad_zip(t, t, t | (any -> any), t | (any -> any)) :: [{any, any}]
+  def pad_zip(enumerable1, enumerable2, padding1, padding2)
+
+  def pad_zip(enum1, enum2, pad1, pad2) do
+    count1 = Enum.count(enum1)
+    count2 = Enum.count(enum2)
+    
+    cond do
+      count1 < count2 ->
+        enum1 = pad(enum1, count2, pad1)
+      count1 > count2 ->
+        enum2 = pad(enum2, count1, pad2)
+      true ->
+        # counts are equal, just zip it
+        true
+    end
+
+    Enum.zip(enum1, enum2)
+  end
 end
--- test/enum_pad_test.exs
+++ test/enum_pad_test.exs
@@ -2,78 +2,154 @@ defmodule Experimental.EnumPadTest do
   use ExUnit.Case, async: true
 
   require Logger
-  alias Experimental.EnumPad, as: Enum
+  import Experimental.EnumPad
 
   doctest Experimental.EnumPad, import: true
 
-  #@tag :skip
-  test "pad: size > length of collection" do
-    col = [1, 2, 3]
-    assert Enum.pad(col, 5)  == [1, 2, 3, nil, nil]
-    assert Enum.pad(col, 5, 42)  == [1, 2, 3, 42, 42]
-    assert Enum.pad(col, 5, &(&1+10))  == [1, 2, 3, 13, 23]
+  defp build_emumerables(range) do
+    [ range,
+      Enum.to_list(range),
+      Stream.into(range, []),
+    ]
   end
 
-  #@tag :skip
-  test "pad: size < length of collection" do
-    col = [1, 2, 3, 4, 5]
-    assert Enum.pad(col, 3)  == col
-    assert Enum.pad(col, 3, 42)  == col
-    assert Enum.pad(col, 3, &(&1+10))  == col
+  @empty_enumerables [%{}, []]
+
+  defp cyc(list) do
+    Stream.cycle(list)
   end
 
-  #@tag :skip
-  test "pad: size == length of collection" do
-    col = [1, 2, 3, 4, 5]
-    assert Enum.pad(col, 5)  == col
-    assert Enum.pad(col, 5, 42)  == col
-    assert Enum.pad(col, 5, &(&1+10))  == col
+  test "enumerable?" do
+    assert enumerable?([])
+    assert enumerable?([1])
+    assert enumerable?([1, 2, 3])
+    assert enumerable?(1..10)
+    assert enumerable?(%{})
+    assert enumerable?(%{a: 1, b: 2})
+    assert enumerable?([a: 1])
+    assert enumerable?([a: 1, b: 2])
+    assert enumerable?(Stream.cycle([nil]))
+    assert enumerable?(Stream.into(1..10, []))
+    
+    refute enumerable?(nil)
+    refute enumerable?(1)
+    refute enumerable?({})
+    refute enumerable?({1})
+    refute enumerable?({1, 2})
+    refute enumerable?(&(&1))
+    refute enumerable?(fn -> [] end)
   end
 
-  #@tag :skip
-  test "pad: 1 element collection" do
-    col = [10000]
-    assert Enum.pad(col, 0)  == col
-    assert Enum.pad(col, -100)  == col
-    assert Enum.pad(col, 1)  == col
-    assert Enum.pad(col, 5)  == [10000, nil, nil, nil, nil]
-    assert Enum.pad(col, 5, fn(x) -> x + 10 end)  == [10000, 10010, 10020, 10030, 10040]
+  test "pad: size > length of enumerable" do
+    for enum <- build_emumerables(1..3) do
+      assert pad(enum, 5)            == [1, 2, 3, nil, nil]
+      assert pad(enum, 5, cyc([42])) == [1, 2, 3, 42, 42]
+      assert pad(enum, 5, &(&1+10))  == [1, 2, 3, 13, 23]
+    end
   end
 
-  #@tag :skip
-  test "pad: empty collection" do
-    assert Enum.pad([], 5)  == []
-    assert Enum.pad([], 5, 42)  == []
-    assert Enum.pad([], 5, &(&1+10))  == []
+  test "pad: size < length of enumerable" do
+    for enum <- build_emumerables(1..5) do
+      assert pad(enum, 3)            == Enum.to_list(enum)
+      assert pad(enum, 3, cyc([42])) == Enum.to_list(enum)
+      assert pad(enum, 3, &(&1+10))  == Enum.to_list(enum)
+    end
   end
 
-  #@tag :skip
+  test "pad: size == length of enumerable" do
+    for enum <- build_emumerables(1..5) do
+      assert pad(enum, 5)            == Enum.to_list(enum)
+      assert pad(enum, 5, cyc([42])) == Enum.to_list(enum)
+      assert pad(enum, 5, &(&1+10))  == Enum.to_list(enum)
+    end
+  end
+
+  test "pad: size == +1 size of enumerable" do
+    for enum <- build_emumerables(1..5) do
+      assert pad(enum, 6)           == [1, 2, 3, 4, 5, nil]
+      assert pad([], 1, cyc([42]))  == [42]
+    end
+  end
+
+  test "pad: 1 element enumerable" do
+    for enum <- build_emumerables(10000..10000) do
+      assert pad(enum, 0)    == Enum.to_list(enum)
+      assert pad(enum, -100) == Enum.to_list(enum)
+      assert pad(enum, 1)    == Enum.to_list(enum)
+      assert pad(enum, 5) 
+        == [10000, nil, nil, nil, nil]
+      assert pad(enum, 5, fn(x) -> x + 10 end)
+        == [10000, 10010, 10020, 10030, 10040]
+    end
+  end
+
+  test "pad: empty enumerable" do
+    for enum <- @empty_enumerables do
+      assert pad(enum, 5)            == [nil, nil, nil, nil, nil]
+      assert pad(enum, 5, cyc([42])) == [42, 42, 42, 42, 42]
+      assert pad(enum, 1, cyc([0]))  == [0]
+      assert pad(enum, 5, fn
+        nil -> 0
+        x -> x + 10
+      end) == [0, 10, 20, 30, 40]
+    end
+  end
+
+  test "pad: size == 0" do
+    for enum <- build_emumerables(1..3) do
+      assert pad(enum, 0)            == Enum.to_list(enum)
+      assert pad(enum, 0, cyc([42])) == Enum.to_list(enum)
+      assert pad(enum, 0, &(&1+10))  == Enum.to_list(enum)
+    end
+
+    for enum <- @empty_enumerables do
+      assert pad(enum, 0)            == []
+      assert pad(enum, 0, cyc([42])) == []
+      assert pad(enum, 0, &(&1+10))  == []
+    end
+  end
+
   test "pad: negative size" do
-    col = [1, 2, 3, 4, 5]
-    assert Enum.pad(col, 0)  == col
-    assert Enum.pad(col, -100)  == col
-    assert Enum.pad(col, -100, &(&1+10))  == col
+    for enum <- build_emumerables(1..5) do
+      assert pad(enum, -100)           == Enum.to_list(enum)
+      assert pad(enum, -100, &(&1+10)) == Enum.to_list(enum)
+    end
   end
 
-  #@tag :skip
   test "pad: check optimization" do
-    col = 1..1_000_000 |> :"Elixir.Enum".to_list
-    assert Enum.pad(col, 1_000_005, &(&1+10))  == 
-      :"Elixir.Enum".reverse( [1_000_050, 1_000_040, 1_000_030, 1_000_020, 1_000_010 | :"Elixir.Enum".reverse(col)] )
+    for enum <- build_emumerables(1..1_000_000) do
+      assert pad(enum, 1_000_005, &(&1+10)) == 
+        :"Elixir.Enum".reverse(
+          [1_000_050, 1_000_040, 1_000_030, 1_000_020, 1_000_010
+          | :"Elixir.Enum".reverse(enum)])
+        |> Enum.to_list
+    end
   end
 
-  # test "pad: range" do
-  #   col = 1..3
-  #   assert Enum.pad(col, 5)  == [1, 2, 3, nil, nil]
-  #   assert Enum.pad(col, 5, 42)  == [1, 2, 3, 42, 42]
-  #   assert Enum.pad(col, 5, &(&1+10))  == [1, 2, 3, 13, 23]
-  # end
+  test "pad: range" do
+    for enum <- build_emumerables(1..3) do
+      assert pad(enum, 5)            == [1, 2, 3, nil, nil]
+      assert pad(enum, 5, cyc([42])) == [1, 2, 3, 42, 42]
+      assert pad(enum, 5, &(&1+10))  == [1, 2, 3, 13, 23]
+    end
+  end
 
-  # test "pad: map" do
-  #   col = %{a: 1, b: 2, c: 3}
-  #   assert Enum.pad(col, 5)  == [{:a, 1}, {:b, 2}, {:c, 3}, nil, nil]
-  #   assert Enum.pad(col, 5, 42)  == [{:a, 1}, {:b, 2}, {:c, 3}, 42, 42]
-  #   #assert Enum.pad(col, 5, &(&1+10))  == [{:a, 1}, {:b, 2}, {:c, 3}, 13, 23]
-  # end
+  test "pad: map and keyword list" do
+    enums = [
+      %{a: 1, b: 2, c: 3},
+      [a: 1, b: 2, c: 3]
+    ]
+    for enum <- enums do
+      assert pad(enum, 5)
+        == [{:a, 1}, {:b, 2}, {:c, 3}, nil, nil]
+      assert pad(enum, 5, cyc([42]))
+        == [{:a, 1}, {:b, 2}, {:c, 3}, 42, 42]
+      assert pad(enum, 5, fn({_k,v}) -> {nil, v + 10} end)
+        == [{:a, 1}, {:b, 2}, {:c, 3}, {nil, 13}, {nil, 23}]
+    end
+  end
 
+  test "pad: edge cases" do
+    assert pad([:a], 2, cyc([0])) == [:a, 0]
+  end
 end
